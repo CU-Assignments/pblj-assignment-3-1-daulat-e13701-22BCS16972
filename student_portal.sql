d0a98bf76afd239c7928b41806e18405e00ec00c
22BCS16972 <22BCS16972@cuchd.in>
2025-04-13 23:08:25 +0530

    Create student_portal.sql

---
+++ student_portal.sql
@@ -0,0 +1,5 @@
+create database student_portal;
+
+use student_portal;
+
+create table attendance (ID int auro_increment primary key, Student_Name varchar(100), Subject varchar(100), Date date, Status varchar(20));
